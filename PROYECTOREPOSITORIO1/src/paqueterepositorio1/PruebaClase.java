8f4c5ea3336007f0d65f7ea9d4795e96069666b7
atore <atore@Antonio-1>
2026-02-10 09:11:02 +0100

    Prueba

--- PROYECTOREPOSITORIO1/src/paqueterepositorio1/PruebaClase.java
+++ PROYECTOREPOSITORIO1/src/paqueterepositorio1/PruebaClase.java
@@ -1,6 +1,10 @@
 package paqueterepositorio1;
 
 public class PruebaClase {
-String texto = "Hola Mundo";
+	static String texto = "Hola Mundo";
 
+	public static void main(String[] args) {
+		System.out.println(texto);	
+	}
 }
+
